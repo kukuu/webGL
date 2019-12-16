7fe88510b97cb5e59b1945eb1efcb73e11db4348
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-12-16 13:05:08 +0000

    Attached fragment shader.
    Interpolation
    ........................
    1. vec4 is  set to 4 inputs  - RGBA
    2. Inputs are passed directly (1, 0, 0, 1), not through arguments (position, 1).

--- public/main.js
+++ public/main.js
@@ -37,12 +37,28 @@ const buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 gl.bufferData(gl.ARRAY_BUFFER,  new Float32Array(vertexData), gl.STATIC_DRAW);
 
+
+//Create vertex shader
+//pass resource from GPU as string. This is not provided by JavaScript
+//Paased as a string
+//This is void as no return is expected
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shadeSource(vertexShader, 
-	attribute vec3 position;
+	`attribute vec3 position;
 	void main() {
 		gl_Position = vec4(position, 1)
 	}
-);
+	`);
 
 gl.compileShader(vertexShader);
+
+
+//Create fragment shader
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shadeSource(fragmentShader, `
+	void main() {
+		gl_FragColor = vec4(1, 0, 0, 1)
+	}
+	`);
+
+gl.compileShader(fragmentShader);
